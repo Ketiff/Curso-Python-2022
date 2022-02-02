7886cba01ce1f16a0a291e6c3f2efc332674dec5
Revelo <kevrevelo@hotmail.com>
2022-02-01 19:05:20 -0500

    inicio

---
+++ Fundamentos/0.HolaMundo.py
@@ -0,0 +1,11 @@
+# Script: Un conjunto de instrucciones a ser ejecutadas.
+
+#Salida de datos
+#print('Formulario')
+#Entrada de datos
+#input()
+
+print ('Formulario')
+print ('Ingrese tu nombre')
+nombre = input()
+print ('El nombre ingresado es: ', nombre)
