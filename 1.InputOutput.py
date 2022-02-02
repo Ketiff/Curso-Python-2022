c09e15d3b787a64f3cf45f2e10254f965e3bbdb2
Revelo <kevrevelo@hotmail.com>
2022-02-01 20:01:34 -0500

    Segundo

---
+++ 1.InputOutput.py
@@ -0,0 +1,24 @@
+#Funcion de entrada y funcion de salida
+#Va a buscar las palabras reservadas que Python provee
+#Python es un lenguaje 100% orientado a objetos
+
+
+#Argumento a la funcion '' -> string
+#Funcion input() -> Entrada de datos
+#end='\n' -> Por defecto
+
+print('Formulario')
+
+print('Nombre: ', end='')
+nombre = input() 
+print('Apellido: ', end='')
+apellido = input()
+print('Edad: ', end='')
+edad = input()
+ciudad = input('Ingresa tu ciudad: ')
+
+print('Nombre: ',nombre)
+print('Apellido: ',apellido)
+print('Edad: ',edad)
+print('Ciudad: ',ciudad)
+
---
+++ 2.Variables.py
@@ -0,0 +1,19 @@
+'''
+VARIABLES
+>No deben tener caracteres especiales
+>No deben empezar con mayusculas
+>No debe ser una palabra reservada
+'''
+
+#El proceso se llama cast -> casting de variables
+edad = 20 #int
+nombre = 'Kevin' #string
+ira = 38.29 #La declaracion de los flotantes es con punto
+mayorEdad = True
+
+print('El tipo de dato de la variable edad es: ', type(edad)) #type nos devuelve el tipo de valor
+print('El tipo de dato de la variable nombre es: ', type(nombre)) 
+print('El tipo de dato de la variable IRA es: ', type(ira)) 
+print('El tipo de dato de la variable mayorEdad es: ', type(mayorEdad))
+
+
